8ef12b8108bad7f21a63efe6b6b95fbfcea02046
mehmetgencv <mehmetgencv@gmail.com>
2024-01-07 00:49:31 +0300

    add OrderService

---
+++ order-service/src/main/java/com/shoppingtime/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.shoppingtime.orderservice.service;
+
+import com.shoppingtime.orderservice.dto.OrderLineItemsDto;
+import com.shoppingtime.orderservice.dto.OrderRequest;
+import com.shoppingtime.orderservice.model.Order;
+import com.shoppingtime.orderservice.model.OrderLineItems;
+import com.shoppingtime.orderservice.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItems(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems(
+                orderLineItemsDto.getId(),
+                orderLineItemsDto.getSkuCode(),
+                orderLineItemsDto.getPrice(),
+                orderLineItemsDto.getQuantity()
+        );
+        return orderLineItems;
+    }
+}
