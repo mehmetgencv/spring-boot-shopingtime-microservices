e7bf89d8bd53f1ab0c4e6a6021665855dd769ae0
mehmetgencv <mehmetgencv@gmail.com>
2024-01-08 23:24:48 +0300

    add slf4j annotation

--- inventory-service/src/main/java/com/shoppingtime/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/shoppingtime/inventoryservice/controller/InventoryController.java
@@ -2,6 +2,7 @@ package com.shoppingtime.inventoryservice.controller;
 
 import com.shoppingtime.inventoryservice.dto.InventoryResponse;
 import com.shoppingtime.inventoryservice.service.InventoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
+@Slf4j
 public class InventoryController {
     private final InventoryService inventoryService;
 
