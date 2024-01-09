f09b39e06801394a17f6aeb4b89c5a423e7b4989
mehmetgencv <mehmetgencv@gmail.com>
2024-01-09 21:55:44 +0300

    update application.properties for discovery server

--- order-service/src/main/java/com/shoppingtime/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/shoppingtime/orderservice/controller/OrderController.java
@@ -2,12 +2,14 @@ package com.shoppingtime.orderservice.controller;
 
 import com.shoppingtime.orderservice.dto.OrderRequest;
 import com.shoppingtime.orderservice.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
